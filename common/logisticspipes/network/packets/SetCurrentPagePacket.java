33ff270a39590c21ce0c7f1bc6764a492f5b2167
Bernasss12 <tudo.pt.minecraft@gmail.com>
2019-06-11 23:23:21 +0100

    Remade some key parts
    - Routed the GUI opening through the ClientProxy.
    - Reformatted the packet to not use NBT but instead simple values.
    - Made it work _flawlessly_ with the offhand.

--- common/logisticspipes/network/packets/SetCurrentPagePacket.java
+++ common/logisticspipes/network/packets/SetCurrentPagePacket.java
@@ -4,23 +4,35 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.EnumHand;
+<<<<<<< feature/custom-guide-book
 import net.minecraft.util.ResourceLocation;
 
 import net.minecraftforge.fml.common.registry.GameRegistry;
+=======
+>>>>>>> Remade some key parts
 
 import lombok.Getter;
 import lombok.Setter;
 import sun.rmi.runtime.Log;
 
+<<<<<<< feature/custom-guide-book
 import logisticspipes.LPConstants;
 import logisticspipes.LPItems;
 import logisticspipes.LogisticsPipes;
 import logisticspipes.items.ItemGuideBook;
 import logisticspipes.items.LogisticsItem;
+=======
+import logisticspipes.LogisticsPipes;
+import logisticspipes.items.ItemGuideBook;
+>>>>>>> Remade some key parts
 import logisticspipes.network.abstractpackets.ModernPacket;
 import logisticspipes.utils.StaticResolve;
 import network.rs485.logisticspipes.util.LPDataInput;
 import network.rs485.logisticspipes.util.LPDataOutput;
+
+/**
+ * TODO make this right
+ */
 
 @StaticResolve
 public class SetCurrentPagePacket extends ModernPacket {
@@ -44,6 +56,7 @@ public class SetCurrentPagePacket extends ModernPacket {
 	@Override
 	public void processPacket(EntityPlayer player) {
 		ItemStack book;
+<<<<<<< feature/custom-guide-book
 		book = player.getHeldItem(hand);
 		if (book.getItem() != LPItems.itemGuideBook) return;
 		NBTTagCompound nbt = book.getTagCompound();
@@ -51,6 +64,18 @@ public class SetCurrentPagePacket extends ModernPacket {
 		nbt.setFloat("sliderProgress", sliderProgress);
 		nbt.setInteger("page", page);
 		book.setTagCompound(nbt);
+=======
+		if (hand == EnumHand.MAIN_HAND) {
+			if (!(player.getHeldItemMainhand().getItem().getClass() == ItemGuideBook.class)) return;
+			book = player.getHeldItemMainhand();
+		} else {
+			if (!(player.getHeldItemOffhand().getItem().getClass() == ItemGuideBook.class)) return;
+			book = player.getHeldItemOffhand();
+		}
+		NBTTagCompound nbt = book.getTagCompound();
+		nbt.setFloat("sliderProgress", sliderProgress);
+		nbt.setInteger("page", page);
+>>>>>>> Remade some key parts
 	}
 
 	@Override
