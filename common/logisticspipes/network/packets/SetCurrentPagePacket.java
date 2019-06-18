9a4c527d9feeabc74d1ca008feceead76360a848
Bernasss12 <tudo.pt.minecraft@gmail.com>
2019-06-18 18:33:52 +0100

    Lots of additions
     - Reworked tiles
     - New expandable file reading structure
     - New Menu with tiles and Title boxes
     - New PageState system (not renamed yet but functioning)
     - New tile Title overlay
     - New Buttons (home, next/prev page)
    and maybe some more little things I can't recall right now :)

--- common/logisticspipes/network/packets/SetCurrentPagePacket.java
+++ common/logisticspipes/network/packets/SetCurrentPagePacket.java
@@ -4,41 +4,9 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.EnumHand;
-<<<<<<< feature/custom-guide-book
-<<<<<<< feature/custom-guide-book
-import net.minecraft.util.ResourceLocation;
-
-import net.minecraftforge.fml.common.registry.GameRegistry;
-=======
->>>>>>> Remade some key parts
-=======
-import net.minecraft.util.ResourceLocation;
-
-import net.minecraftforge.fml.common.registry.GameRegistry;
->>>>>>> Formatting fixes
-
 import lombok.Getter;
 import lombok.Setter;
-import sun.rmi.runtime.Log;
-
-<<<<<<< feature/custom-guide-book
-<<<<<<< feature/custom-guide-book
-import logisticspipes.LPConstants;
 import logisticspipes.LPItems;
-import logisticspipes.LogisticsPipes;
-import logisticspipes.items.ItemGuideBook;
-import logisticspipes.items.LogisticsItem;
-=======
-import logisticspipes.LogisticsPipes;
-import logisticspipes.items.ItemGuideBook;
->>>>>>> Remade some key parts
-=======
-import logisticspipes.LPConstants;
-import logisticspipes.LPItems;
-import logisticspipes.LogisticsPipes;
-import logisticspipes.items.ItemGuideBook;
-import logisticspipes.items.LogisticsItem;
->>>>>>> Formatting fixes
 import logisticspipes.network.abstractpackets.ModernPacket;
 import logisticspipes.utils.StaticResolve;
 import network.rs485.logisticspipes.util.LPDataInput;
@@ -53,7 +21,7 @@ public class SetCurrentPagePacket extends ModernPacket {
 
 	@Getter
 	@Setter
-	private int page, chapter;
+	private int page, chapter, division;
 
 	@Getter
 	@Setter
@@ -66,40 +34,18 @@ public class SetCurrentPagePacket extends ModernPacket {
 	@Override
 	public void processPacket(EntityPlayer player) {
 		ItemStack book;
-<<<<<<< feature/custom-guide-book
-<<<<<<< feature/custom-guide-book
 		book = player.getHeldItem(hand);
 		if (book.getItem() != LPItems.itemGuideBook) return;
-		NBTTagCompound nbt = book.getTagCompound();
-		if(nbt == null) nbt = new NBTTagCompound();
-		nbt.setFloat("sliderProgress", sliderProgress);
-		nbt.setInteger("page", page);
-		book.setTagCompound(nbt);
-=======
-		if (hand == EnumHand.MAIN_HAND) {
-			if (!(player.getHeldItemMainhand().getItem().getClass() == ItemGuideBook.class)) return;
-			book = player.getHeldItemMainhand();
-		} else {
-			if (!(player.getHeldItemOffhand().getItem().getClass() == ItemGuideBook.class)) return;
-			book = player.getHeldItemOffhand();
+		NBTTagCompound nbt;
+		if(book.hasTagCompound()){
+			nbt = book.getTagCompound();
+		}else{
+			nbt = new NBTTagCompound();
 		}
-=======
-		book = player.getHeldItem(hand);
-		if (book.getItem() != LPItems.itemGuideBook) return;
->>>>>>> Formatting fixes
-		NBTTagCompound nbt = book.getTagCompound();
-		if(nbt == null) nbt = new NBTTagCompound();
 		nbt.setFloat("sliderProgress", sliderProgress);
 		nbt.setInteger("page", page);
-<<<<<<< feature/custom-guide-book
-<<<<<<< feature/custom-guide-book
->>>>>>> Remade some key parts
-=======
-=======
 		nbt.setInteger("chapter", chapter);
->>>>>>> Major changes to the rendering and book structure
-		book.setTagCompound(nbt);
->>>>>>> Ready for Review
+		nbt.setInteger("division", division);
 	}
 
 	@Override
@@ -109,6 +55,7 @@ public class SetCurrentPagePacket extends ModernPacket {
 		sliderProgress = input.readFloat();
 		page = input.readInt();
 		chapter = input.readInt();
+		division = input.readInt();
 	}
 
 	@Override
@@ -118,6 +65,7 @@ public class SetCurrentPagePacket extends ModernPacket {
 		output.writeFloat(sliderProgress);
 		output.writeInt(page);
 		output.writeInt(chapter);
+		output.writeInt(division);
 	}
 
 	@Override
